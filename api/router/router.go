0e95c3118ef75c767945a9ce1f9c1f4b337a0160
Alexander Kupriyanov <sanya-spb@mail.ru>
2021-09-06 15:17:37 +0300

    task05

--- api/router/router.go
+++ api/router/router.go
@@ -3,17 +3,39 @@ package router
 import (
 	"net/http"
 
+	"github.com/go-chi/chi/v5"
+	"github.com/sanya-spb/URL-shortener/api/auth"
 	"github.com/sanya-spb/URL-shortener/api/handler"
 )
 
 type Router struct {
 	http.Handler
-	hs *handler.Handler
+	hHandler *handler.Handler
 }
 
-func NewRouter(hs *handler.Handler) *Router {
+func NewRouter(hHandler *handler.Handler) *Router {
 	rRouter := &Router{
-		hs: hs,
+		hHandler: hHandler,
 	}
+
+	r := chi.NewRouter()
+	r.Group(func(rAdm chi.Router) {
+		rAdm.Get("/stat", rRouter.Stat)
+		rAdm.Delete("/delete", rRouter.Delete)
+	}).Use(auth.AuthMiddleware) // TODO: заменить на middleware.BasicAuth(..)
+	r.Put("/new", rRouter.Create)
+	r.Get("/info", rRouter.Read)
+	r.Put("/update", rRouter.Update)
+	// r.Delete("/delete", rRouter.Delete)
+	r.Get("/go", rRouter.Go)
+	// r.Get("/stat", rRouter.Stat)
+
 	return rRouter
 }
+
+func (rRouter *Router) Create(w http.ResponseWriter, r *http.Request)
+func (rRouter *Router) Read(w http.ResponseWriter, r *http.Request)
+func (rRouter *Router) Update(w http.ResponseWriter, r *http.Request)
+func (rRouter *Router) Delete(w http.ResponseWriter, r *http.Request)
+func (rRouter *Router) Go(w http.ResponseWriter, r *http.Request)
+func (rRouter *Router) Stat(w http.ResponseWriter, r *http.Request)
--- go.mod
+++ go.mod
@@ -8,4 +8,7 @@ require (
 	gopkg.in/yaml.v3 v3.0.0-20210107192922-496545a6307b
 )
 
-require github.com/pkg/errors v0.9.1 // indirect
+require (
+	github.com/go-chi/chi/v5 v5.0.4 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
+)
