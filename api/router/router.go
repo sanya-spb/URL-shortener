d7d4d24cca1bc50cae623d0705aec91214c6948e
Alexander Kupriyanov <sanya-spb@mail.ru>
2021-09-07 13:39:46 +0300

    chi define endpoints /put, /info

--- api/router/router.go
+++ api/router/router.go
@@ -4,6 +4,8 @@ import (
 	"net/http"
 
 	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/render"
+	"github.com/google/uuid"
 	"github.com/sanya-spb/URL-shortener/api/auth"
 	"github.com/sanya-spb/URL-shortener/api/handler"
 )
@@ -12,6 +14,11 @@ type Router struct {
 	http.Handler
 	hHandler *handler.Handler
 }
+
+type TLink handler.TLink
+
+func (TLink) Bind(r *http.Request) error
+func (TLink) Render(w http.ResponseWriter, r *http.Request) error
 
 func NewRouter(hHandler *handler.Handler) *Router {
 	rRouter := &Router{
@@ -22,20 +29,48 @@ func NewRouter(hHandler *handler.Handler) *Router {
 	r.Group(func(rAdm chi.Router) {
 		rAdm.Get("/stat", rRouter.Stat)
 		rAdm.Put("/update", rRouter.Update)
-		rAdm.Delete("/delete", rRouter.Delete)
+		rAdm.Delete("/delete/{uuid}", rRouter.Delete)
 	}).Use(auth.AuthMiddleware) // TODO: заменить на middleware.BasicAuth()
 	r.Put("/new", rRouter.Create)
-	r.Get("/info", rRouter.Read)
+	r.Get("/info/{uuid}", rRouter.Read)
 	// r.Put("/update", rRouter.Update)
 	// r.Delete("/delete", rRouter.Delete)
-	r.Get("/go", rRouter.Go)
+	r.Get("/go/{id}", rRouter.Go)
 	// r.Get("/stat", rRouter.Stat)
 
 	return rRouter
 }
 
-func (rRouter *Router) Create(w http.ResponseWriter, r *http.Request)
-func (rRouter *Router) Read(w http.ResponseWriter, r *http.Request)
+func (rRouter *Router) Create(w http.ResponseWriter, r *http.Request) {
+	rl := TLink{}
+	if err := render.Bind(r, &rl); err != nil {
+		render.Render(w, r, Err400(err))
+		return
+	}
+	l, err := rRouter.hHandler.Create(r.Context(), handler.TLink(rl))
+	if err != nil {
+		render.Render(w, r, Err500(err))
+		return
+	}
+	render.Render(w, r, TLink(l))
+}
+
+func (rRouter *Router) Read(w http.ResponseWriter, r *http.Request) {
+	id := chi.URLParam(r, "uuid")
+
+	lid, err := uuid.Parse(id)
+	if err != nil {
+		render.Render(w, r, Err400(err))
+		return
+	}
+	l, err := rRouter.hHandler.Read(r.Context(), lid)
+	if err != nil {
+		render.Render(w, r, Err500(err))
+		return
+	}
+	render.Render(w, r, TLink(l))
+}
+
 func (rRouter *Router) Update(w http.ResponseWriter, r *http.Request)
 func (rRouter *Router) Delete(w http.ResponseWriter, r *http.Request)
 func (rRouter *Router) Go(w http.ResponseWriter, r *http.Request)
--- go.mod
+++ go.mod
@@ -10,5 +10,6 @@ require (
 
 require (
 	github.com/go-chi/chi/v5 v5.0.4 // indirect
+	github.com/go-chi/render v1.0.1
 	github.com/pkg/errors v0.9.1 // indirect
 )
